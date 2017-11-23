ca9de67ee2e7c0c30031b496b2712ccdc55c6b10
CaoZGit <1074401754@qq.com>
2017-11-23 22:06:44 +0800

    添加icon

---
+++ MeiZi/app/src/main/java/com/cz/meizi/App.java
@@ -0,0 +1,18 @@
+package com.cz.meizi;
+
+import android.app.Application;
+
+/**
+ * @author 10744
+ * @date 2017/11/23
+ * <p>
+ * describe:
+ */
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
